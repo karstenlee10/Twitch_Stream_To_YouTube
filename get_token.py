4d2a1a04b266baf24d48308342b7b03bd900d9c7
karstenlee <91263511+karstenlee10@users.noreply.github.com>
2025-01-28 14:25:44 +0800

    Update get_token.py
    added gmail

--- get_token.py
+++ get_token.py
@@ -1,18 +1,6 @@
-import sys
-import json
-import os
-import threading
-import time
-import cv2
-import subprocess
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import logging
-from io import StringIO
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.credentials import Credentials
 
 APP_TOKEN_FILE = "client_secret.json"
 USER_TOKEN_FILE = "user_token.json"
@@ -20,18 +8,26 @@ USER_TOKEN_FILE = "user_token.json"
 SCOPES = [
     'https://www.googleapis.com/auth/youtube.force-ssl',
     'https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/gmail.readonly',
 ]
 
 def get_creds_saved():
     creds = None
 
     # Create new credentials via local server flow
-    flow = InstalledAppFlow.from_client_secrets_file(APP_TOKEN_FILE, SCOPES)
-    
-    creds = flow.run_local_server(port=6971)
+    flow = InstalledAppFlow.from_client_secrets_file(APP_TOKEN_FILE, SCOPES, redirect_uri='urn:ietf:wg:oauth:2.0:oob')
+   
+    # Try to load existing user token
+    try:
+      creds = Credentials.from_authorized_user_file(USER_TOKEN_FILE)
+    except FileNotFoundError:
+       pass
 
-    with open(USER_TOKEN_FILE, 'w') as token:
-        token.write(creds.to_json())
+    if not creds or not creds.valid:
+        creds = flow.run_local_server(port=6971)
+
+        with open(USER_TOKEN_FILE, 'w') as token:
+            token.write(creds.to_json())
 
 if __name__ == '__main__':
     logging.basicConfig(filename="get_token.log", level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
