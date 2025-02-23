d0e5c279ce5f3f8df601ac6ffca3ba72bb76a30e
karstenlee <91263511+karstenlee10@users.noreply.github.com>
2025-02-23 16:48:47 +0800

    Update config_tv.py

--- config_tv.py
+++ config_tv.py
@@ -1,19 +1,35 @@
+##########################################################################
+#ARCHIVING TWITCH OR BILIBILI
 Twitch = "True" #twitch or bilibili
 BiliBili = "False" #twitch or bilibili
-Chrome_Profile = "" #chrome profile folder name
+##########################################################################
+#ARCHIVE STREAMER USERNAME
 username = "" #twitch or bilibili username
+##########################################################################
+#ARCHIVE SETTINGS
 ytshort = "False" #not recommand
 unliststream = "False" #after stream will be public
 disablechat = "True"  #disable chat on live stream
-accountname = "" #google account name(not brand acc have email) when login to google api
-brandacc = "False" #do you use the brand acc as the archive channel
-brandaccname = "Null" #if you have brand acc copy the google account name when login to google api
-ffmpeg1 = "ffmpeg111.exe" #ffmpeg exe name
-ffmpeg = "ffmpeg.exe" #ffmpeg exe name
-apiexe = "ffmpeg_api.exe" #api exe name
+##########################################################################
+#YOUTUBE STUDIO RTMP SETTINGS
 rtmpkeyname1 = "1" + "username" + "mult" #first yt rtmp key name
 rtmpkeyname = "username" #second yt rtmp key name
 rtmp_key = "" #first yt rtmp key
 rtmp_key_1 = "" #second yt rtmp key
+##########################################################################
+#CHROME PROFILE FOLDER NAME
+Chrome_Profile = "" #chrome profile folder name
+##########################################################################
+#API SETTINGS
+accountname = "" #google account name(not brand acc have email) when login to google api
+brandacc = "False" #do you use the brand acc as the archive channel
+brandaccname = "Null" #if you have brand acc copy the google account name when login to google api
+##########################################################################
+#EXE NAME
+ffmpeg1 = "ffmpeg111.exe" #ffmpeg exe name
+ffmpeg = "ffmpeg.exe" #ffmpeg exe name
+apiexe = "ffmpeg_api.exe" #api exe name
+##########################################################################
+#TWITCH API SETTINGS
 client_id = "" #twitch api client id
 client_secret = "" #twitch api client sercet
