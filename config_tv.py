bceeb45f66222820b543a02c5371c9182c13e8cc
karstenlee <91263511+karstenlee10@users.noreply.github.com>
2025-02-27 19:24:13 +0800

    Update config_tv.py

--- config_tv.py
+++ config_tv.py
@@ -1,8 +1,3 @@
-##########################################################################
-#ARCHIVING TWITCH OR BILIBILI
-Twitch = "True" #twitch or bilibili
-BiliBili = "False" #twitch or bilibili
-##########################################################################
 #ARCHIVE STREAMER USERNAME
 username = "" #twitch or bilibili username
 ##########################################################################
@@ -24,12 +19,11 @@ Chrome_Profile = "" #chrome profile folder name
 accountname = "" #google account name(not brand acc have email) when login to google api
 brandacc = "False" #do you use the brand acc as the archive channel
 brandaccname = "Null" #if you have brand acc copy the google account name when login to google api
+#TWITCH API SETTINGS
+client_id = "" #twitch api client id
+client_secret = "" #twitch api client sercet
 ##########################################################################
 #EXE NAME
 ffmpeg1 = "ffmpeg111.exe" #ffmpeg exe name
 ffmpeg = "ffmpeg.exe" #ffmpeg exe name
 apiexe = "ffmpeg_api.exe" #api exe name
-##########################################################################
-#TWITCH API SETTINGS
-client_id = "" #twitch api client id
-client_secret = "" #twitch api client sercet
