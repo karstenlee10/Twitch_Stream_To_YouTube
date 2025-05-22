54fbfe06bc14cb068e313ab613c59aa997c059a9
l5cs <help>
2025-05-22 23:30:08 +0000

    refactor: move 3rd party silencing to config

--- logger.py
+++ logger.py
@@ -2,15 +2,9 @@ import json
 import logging.config
 from typing import Any
 
-from selenium.webdriver.remote.remote_connection import LOGGER as _selenium_logger
-from urllib3.connectionpool import log as _urllib_logger
-
 
 with open("log.config.json") as log_config:
     logging_config: dict[str, Any] = json.load(log_config)  # pyright: ignore[reportExplicitAny,reportAny]
 
-
-_selenium_logger.setLevel(logging.WARNING)
-_urllib_logger.setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 logging.config.dictConfig(logging_config)
