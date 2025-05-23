6279ac7cff4f268a9b674bfbe4c049f0424a39c5
karstenlee <91263511+karstenlee10@users.noreply.github.com>
2025-05-24 04:32:43 +0800

    Update relive_tv.py
    logeeerer

--- relive_tv.py
+++ relive_tv.py
@@ -1,17 +1,19 @@
 import os
 import sys
+from logger_config import check_tv_logger as logging # Importing logging module for logging messages
 import time
-
 import psutil
-import streamlink
-
 import config_tv as config
-from logger import logger
-
+import streamlink
+#sys.path.append('D:/active/twitch_token')
+#import token_key
 
 arguments = sys.argv
-apiexe = "taskkill /f /im " + config.apiexe
-live_link_url = "streamlink https://www.twitch.tv/" + config.username
+apiexe = f"taskkill /f /im {config.apiexe}"
+#twitch_token = token_key.token
+#print(twitch_token)
+live_link_url = f'streamlink https://www.twitch.tv/{config.username}'
+
 def check_is_live():
   trytimes = 0
   while True:
@@ -21,49 +23,47 @@ def check_is_live():
         process_name = config.apiexe
         for process in psutil.process_iter(['pid', 'name']):
           if process.info['name'] == process_name:
-            logger.info('API process is still running')
+            logging.info('api exe still here')
             return "True"
-        logger.info('API process has terminated')
+        logging.info('api exe stop')
         return "False"
     except KeyError:
         trytimes += 1
         time.sleep(5)
         if trytimes == 6:
-            logger.info('Stream has ended - no active broadcast detected')
+            logging.info('The stream is finsh')
             return "False"
 
 def api_this():
-  logger.info('Starting API stream processing')
+  logging.info('script is started now api')
   t = time.localtime()
   current_time = time.strftime("%H:%M:%S", t)
-  command = live_link_url + ' best -o - | ' + config.ffmpeg1 + ' -re -i pipe:0 -c:v copy -c:a aac -ar 44100 -ab 128k -ac 2 -strict -2 -flags +global_header -bsf:a aac_adtstoasc -b:v 6300k -preset fast -f flv rtmp://a.rtmp.youtube.com/live2/' + config.rtmp_key_1
+  command = f"{live_link_url} best -o - | {config.ffmpeg1} -re -i pipe:0 -c:v copy -c:a aac -ar 44100 -ab 128k -ac 2 -strict -2 -flags +global_header -bsf:a aac_adtstoasc -b:v 6300k -preset fast -f flv rtmp://a.rtmp.youtube.com/live2/{config.rtmp_key_1}"
   os.system(command)
   OMG = check_is_live()
   if OMG == "True":
-    logger.info("Stream interrupted - initiating restart procedure")
+    logging.info("steam got down restart")
     api_this()
   else:
-    logger.info('Stream completed - terminating process and cleanup')
+    logging.info('stream has finish no loop it and kill countdown')
     os.system(apiexe)
 
 def this():
-  logger.info('Initializing stream processing')
+  logging.info('script is started now')
   t = time.localtime()
   current_time = time.strftime("%H:%M:%S", t)
-  command = live_link_url + ' best -o - | ' + config.ffmpeg + ' -re -i pipe:0 -c:v copy -c:a aac -ar 44100 -ab 128k -ac 2 -strict -2 -flags +global_header -bsf:a aac_adtstoasc -b:v 6300k -preset fast -f flv rtmp://a.rtmp.youtube.com/live2/' + config.rtmp_key
+  command = f"{live_link_url} best -o - | {config.ffmpeg} -re -i pipe:0 -c:v copy -c:a aac -ar 44100 -ab 128k -ac 2 -strict -2 -flags +global_header -bsf:a aac_adtstoasc -b:v 6300k -preset fast -f flv rtmp://a.rtmp.youtube.com/live2/{config.rtmp_key}"
   os.system(command)
   OMG = check_is_live()
   if OMG == "True":
-    logger.info("Stream interrupted - initiating restart procedure")
+    logging.info("steam got down restart")
     this()
   else:
-    logger.info('Stream completed - terminating process and cleanup')
+    logging.info('stream has finish no loop it and kill countdown')
     os.system(apiexe)
-
 
 arg1 = arguments[1]
 if arg1 == "api_this":
   api_this()
 if arg1 == "this":
   this()
-exit()
