8fbaa9df74371dd16b14717a94fe3b22adf921c6
karstenlee <91263511+karstenlee10@users.noreply.github.com>
2025-08-29 18:05:27 +0800

    Update config_tv.py

--- config_tv.py
+++ config_tv.py
@@ -2,14 +2,13 @@
 username = "" #twitch or bilibili username
 ##########################################################################
 #ARCHIVE SETTINGS
-playvideo = "True" #if true will play ending screen and error screen
-livestreamautostop = "False" #if true than youtube will auto stop stream
-unliststream = "False" #after stream will be public
-disablechat = "True"  #disable chat on live stream
-playlist = "False"  # True or DOUBLE IF YOU WANT TO SAVE TO MULTIPLE PLAYLIST disable to False
-playlist_id0 = "YOUR_PLAYLIST_ID"  # Replace with your First YouTube playlist ID
-playlist_id1 = "YOUR_PLAYLIST_ID"  # Replace with your actual second YouTube playlist ID
-thumbnail = "False"  # Whether to upload custom thumbnails for streams
+playvideo = True #if true will play ending screen and error screen
+livestreamautostop = False #if true than youtube will auto stop stream
+unliststream = True #after stream will be public
+disablechat = True  #disable chat on live stream
+playlist_id0 = "Null"  # Replace with your First YouTube playlist ID
+playlist_id1 = "Null"  # Replace with your actual second YouTube playlist ID
+thumbnail = False  # Whether to upload custom thumbnails for streams
 ##########################################################################
 #YOUTUBE STUDIO RTMP SETTINGS
 rtmpkeyname1 = "1" + "username" + "mult" #first yt rtmp key name
@@ -22,7 +21,7 @@ Chrome_Profile = "" #chrome profile folder name
 ##########################################################################
 #API SETTINGS
 accountname = "" #google account name(not brand acc have email) when login to google api
-brandacc = "False" #do you use the brand acc as the archive channel
+brandacc = False #do you use the brand acc as the archive channel
 brandaccname = "Null" #if you have brand acc copy the google account name when login to google api
 #TWITCH API SETTINGS
 client_id = "" #twitch api client id
@@ -31,7 +30,6 @@ client_secret = "" #twitch api client sercet
 #EXE NAME
 ffmpeg1 = "ffmpeg111.exe" #ffmpeg exe name
 ffmpeg = "ffmpeg.exe" #ffmpeg exe name
-apiexe = "ffmpeg_api.exe" #api exe name
 ##########################################################################
 #EXPERIMENTAL SETTINGS
 StreamerName = "Null" #if you don't want the twitch username at the title
@@ -42,7 +40,6 @@ ADSqa = False
 Filian = False
 brought_twitch_sub_or_turbo = False #if true will show twitch sub or turbo in description
 public_notification = False #enable public notification and change the stream back to unlisted
-show_twitch_category = True #enable twitch category in description
 ##########################################################################
 # AGREEMENT SETTINGS DON'T SET MANUALLY
 agreement_accepted = False
